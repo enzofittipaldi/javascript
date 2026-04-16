91fc13d45c66335a014ab2a6a5d0e6a4ad67e587
EnzoFittipaldi <enzofittipaldi6@gmail.com>
2026-04-15 22:20:33 -0300

    condições 2.0

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.7
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60.5){ // condição simples! 
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+    console.log(`Dirija sempre usando o cinto de segurança`)
+
