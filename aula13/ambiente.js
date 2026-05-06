c70c38ccf09c040063faac79775d3263806a994f
EnzoFittipaldi <enzofittipaldi6@gmail.com>
2026-05-05 22:04:40 -0300

    Estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 4)
+
+
+
+
+/*
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
